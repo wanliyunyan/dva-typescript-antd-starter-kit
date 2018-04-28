84223ac03408b2587222ebb61c53ea070e15959c
wanliyunyan <88527712pan@163.com>
2018-04-28 10:26:35 +0800

    add theme

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const fs = require('fs');
+const lessToJs = require('less-vars-to-js');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const command = process.argv.slice(2)[0];
 const env = command.substring(command.indexOf('=') + 1);
 const devUrlLoader = 'url-loader?limit=8192&name=[hash:8].[name].[ext]';
 const prodUrlLoader = 'url-loader?limit=8192&name=[hash:8].[name].[ext]&outputPath=assets/images/&publicPath=assets/images';
+
+// 获取自己定义的要覆盖antd默认样式的文件
+const themeVariables = lessToJs(fs.readFileSync(path.join(__dirname, '../src/assets/style/theme.less'), 'utf8'));
 
 module.exports = {
   module: {
@@ -23,10 +28,6 @@ module.exports = {
         use: ExtractTextPlugin.extract({
           use: [{
             loader: 'css-loader',
-            /* options: {
-              modules: true,
-              localIdentName: '[path][name]__[local]--[hash:base64:5]',
-            }, */
           }],
           fallback: 'style-loader',
         }),
@@ -40,10 +41,12 @@ module.exports = {
       },
       {
         test: /\.less$/,
+        exclude: /node_modules/,
         use: ExtractTextPlugin.extract({
           use: [{
             loader: 'css-loader',
             options: {
+              importLoaders: 1,
               modules: true,
               namedExport: true,
               camelCase: true,
@@ -57,7 +60,6 @@ module.exports = {
               sourceMap: true,
               plugins: loader => [
                 require('postcss-import')(),
-                // require('stylelint')(),
                 require('autoprefixer')({
                   browsers: ['last 15 versions'],
                 }),
@@ -68,6 +70,39 @@ module.exports = {
             loader: 'less-loader',
             options: {
               javascriptEnabled: true,
+              modifyVars: themeVariables,
+            },
+          }],
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.less$/,
+        exclude: /src/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              sourceMap: true,
+              plugins: loader => [
+                require('postcss-import')(),
+                require('autoprefixer')({
+                  browsers: ['last 15 versions'],
+                }),
+              ],
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true,
+              modifyVars: themeVariables,
             },
           }],
           fallback: 'style-loader',
