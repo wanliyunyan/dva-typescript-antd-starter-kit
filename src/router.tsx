b5985a5c1a3199a8278c8ed5e65e7a9ed2c3a48e
wanliyunyan <88527712pan@163.com>
2018-11-21 15:30:13 +0800

    add error page

--- src/router.tsx
+++ src/router.tsx
@@ -8,13 +8,13 @@ const { ConnectedRouter } = routerRedux;
 
 export default function({ history }) {
   return (
-    <ConnectedRouter history={history}>
-      <Error>
+    <Error>
+      <ConnectedRouter history={history}>
         <Switch>
           <Route path="/user" component={UserLayout} />
           <Route path="/" component={BasicLayout} />
         </Switch>
-      </Error>
-    </ConnectedRouter>
+      </ConnectedRouter>
+    </Error>
   );
 }
