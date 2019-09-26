9b65f618cb1ddab08a122ab0c96fc6527652bafa
wanliyunyan <88527712pan@163.com>
2019-09-26 13:55:56 +0800

    update config

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -163,7 +163,7 @@ module.exports = function() {
                   enabled: true
                 },
                 pngquant: {
-                  quality: "65-90",
+                  quality: [0.65, 0.9],
                   speed: 4
                 },
                 gifsicle: {
