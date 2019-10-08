af4bf0eea5dfaca888728c65fdd64c275f8cc23e
wanliyunyan <88527712pan@163.com>
2019-10-08 10:24:25 +0800

    update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,6 @@ module.exports = {
     // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "prettier/@typescript-eslint",
     // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    // 此行必须在最后
     "plugin:prettier/recommended"
   ],
   rules: {
