557defccd7457f27c7c14e340728d880fb5c3d89
wanliyunyan <88527712pan@163.com>
2019-12-27 10:47:04 +0800

    update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,66 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  // If you want to use rules which require type information, you will need to specify a path to your tsconfig.json file in the "project" property of "parserOptions"
-  /*parserOptions: {
-    project: "./tsconfig.json"
-  },*/
   env: {
     browser: true,
     es6: true,
     node: true
   },
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       webpack: {
-        config: './build/webpack.base.js',
-      },
+        config: "./build/webpack.base.js"
+      }
     },
+    react: {
+      pragma: "React",
+      version: "detect"
+    }
   },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+    // 'project': './tsconfig.json',
+  },
+  plugins: ["@typescript-eslint", "react", "prettier"],
   extends: [
     // Usage with Airbnb
     "airbnb",
     "plugin:@typescript-eslint/recommended",
-
     // Usage with Prettier
     "prettier",
     // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "prettier/@typescript-eslint",
-    // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    // Enables eslint-plugin-prettier and eslint-config-prettier.
+    // This will display prettier errors as ESLint errors.
+    // Make sure this is always the last configuration in the extends array.
     "plugin:prettier/recommended"
   ],
   rules: {
-    "import/prefer-default-export": 0,
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx",".ts", ".tsx"] }],
-    '@typescript-eslint/explicit-function-return-type': [
-      'off',
+    "import/extensions": [
+      "error",
+      "ignorePackages",
       {
-        allowExpressions: true,
-        allowTypedFunctionExpressions: true,
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never"
       }
     ],
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] }
+    ],
+    "max-classes-per-file": 0,
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "react/jsx-props-no-spreading": 0,
+    "no-useless-escape": 0,
+    "import/prefer-default-export": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "react/jsx-curly-newline": 0,
+    "react/jsx-wrap-multilines": 0
   }
 };
--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,8 @@ module.exports = function(api) {
   const plugins = [
     ["@babel/plugin-proposal-decorators", { legacy: true }],
     ["@babel/plugin-proposal-class-properties", { loose: true }],
+    "@babel/plugin-proposal-nullish-coalescing-operator",
+    "@babel/plugin-proposal-optional-chaining",
     "@babel/plugin-syntax-dynamic-import",
     "@babel/plugin-transform-runtime",
     [
